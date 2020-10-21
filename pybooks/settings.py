db97a1d29aacbb330eb24b9a74d5eaab6e084cd8
vlad-gor <gorunov341@yandex.ru>
2020-10-21 20:20:13 +0300

    migrate to postgres

--- pybooks/settings.py
+++ pybooks/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p_=##h7b0+k#t&b+-21r&kh&2josei363^^qpokv8f6c$%i45s'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,9 +79,17 @@ WSGI_APPLICATION = 'pybooks.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': os.getenv('ENGINE'),
+    'NAME': os.getenv('NAME'),
+    'USER' : os.getenv('USER'),
+    'PASSWORD' : os.getenv('PASSWORD'),
+    'HOST' : os.getenv('HOST'),
+    'PORT' : os.getenv('PORT'),
     }
 }
 
